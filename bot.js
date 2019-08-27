d81fd22267f8abb605f92dc35e8f6218ce18516a
ahmedzzw <54591273+ahmedzzw@users.noreply.github.com>
2019-08-27 17:27:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(`موجود للمساعده لو تاخرت اذا برا البيت`,"http://twitch.tv/ahmed")
 client.user.setStatus("dnd")
 });
 
