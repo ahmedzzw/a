7753fb8cd47074f423366d55e405d5d88a181ed6
ahmedzzw <54591273+ahmedzzw@users.noreply.github.com>
2019-08-28 15:22:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`جميع سرفرات الهاك معطله لا تتطلب اي كود  `,"http://twitch.tv/ahmed")
+client.user.setGame(`خارج المنزل للصلاه وشكرا  `,"http://twitch.tv/ahmed")
 client.user.setStatus("dnd")
 });
 
